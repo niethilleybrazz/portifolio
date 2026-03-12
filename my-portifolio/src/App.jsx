302f98f52e48c35931d180a33097514a80738166
Niethilley Braz <niethilley@gmail.com>
2026-03-12 16:30:46 -0300

    refactor: remove unused App.css and update App component styling

--- my-portifolio/src/App.jsx
+++ my-portifolio/src/App.jsx
@@ -1,9 +1,8 @@
-import './App.css'
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-red-500">Ola Mundo</h1>
+      <h1 className="background-color: var(--color-mint-500)">Ola Mundo</h1>
     </div>
   )
 }
