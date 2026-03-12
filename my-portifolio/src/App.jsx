389c9bda1c1dea107e5d320bce42e36c73c624c1
Niethilley Braz <niethilley@gmail.com>
2026-03-12 16:22:30 -0300

    chore: add @tailwindcss/postcss as a dev dependency in package.json

--- my-portifolio/src/App.jsx
+++ my-portifolio/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <div>
-
+      <h1 className="text-3xl font-bold underline text-red-500">Ola Mundo</h1>
     </div>
   )
 }
