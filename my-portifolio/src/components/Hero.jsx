922e5d5fafedc68699eb41fc86e62c2102530670
Niethilley Braz <niethilley@gmail.com>
2026-03-31 09:58:27 -0300

    feat: adiciona imagem ao hero e icones de navegacao

--- my-portifolio/src/components/Hero.jsx
+++ my-portifolio/src/components/Hero.jsx
@@ -1,17 +1,24 @@
-import chromeStar from '../assets/chromeStar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Hero = () => {
   return (
     <div>
-        <div className="min-h-[70vh] flex flex-col items-center justify-center text-left">
-        <div>
-          <h1 className="max-w-155 text-font font-main text-4xl font-medium"> Oie, eu sou a <span className="bg-linear-to-r from-purple via-blue-400 to-blue bg-clip-text text-transparent font-bold">Niethilley</span> e sou desenvolvedora full-stack</h1>
-          <h2 className="text-xl text-borders font-others">Movida a café e apaixonada por tecnologia.</h2>
+        <div className="min-h-[70vh] flex items-center justify-center text-left">
+          <div>
+            <h1 className="max-w-133 text-font font-main text-4xl font-medium"> Oie, eu sou a <span className="bg-linear-to-r from-purple via-blue-400 to-blue bg-clip-text text-transparent font-bold">Niethilley</span> e sou desenvolvedora full-stack</h1>
+            <h2 className="text-xl text-borders font-others">Movida a café e apaixonada por tecnologia.</h2>
+          </div>
+
+          <div>
+            <img src="/freepik_0001.png" alt="Chrome Star" className="w-[120px]" />
+          </div>
         </div>
-        <div>
-          <img src="{chromeStar}" alt="Chrome Star" />
+
+        <div className='justify-center flex'> 
+          <FontAwesomeIcon icon={faGithub} className="text-font w-12 h-12" />
+          <FontAwesomeIcon icon={faLinkedin} className="text-font w-12 h-12" />
         </div>
-      </div>
     </div>
   )
 }
