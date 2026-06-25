be216b3304402b755ac8e31d5de300a64246867d
Niethilley Braz <niethilley@gmail.com>
2026-06-25 11:41:48 -0300

    feat: enhance Projects and ProjectCard components with technology stacks and improved interactivity; update CSS for new color variables

--- my-portifolio/src/components/Projects.jsx
+++ my-portifolio/src/components/Projects.jsx
@@ -1,35 +1,65 @@
-import React from 'react'
+import React from "react";
 import { TiArrowForwardOutline } from "react-icons/ti";
-import ProjectCard from './ui/ProjectCard';
+import ProjectCard from "./ui/ProjectCard";
 
 const Projects = () => {
-    const projectsData= [
-        { id: 1, category: "E-comerce", title: "Loja de Roupas", description: "Lorem ipsum dolor sit amet...Lorem ipsum dolor sit amet...Lorem ipsum dolor sit amet...Lorem ipsum dolor sit amet...Lorem ipsum dolor sit amet...", image: "url1.jpg"},
-        { id: 2, category: "E-commerce", title: "App Delivery", description: "Lorem ipsum dolor sit amet...", image: "url2.jpg" },
-        { id: 3, category: "E-commerce", title: "SaaS Dashboard", description: "Lorem ipsum dolor sit amet...", image: "url3.jpg" }
-    ]
+  const projectsData = [
+    {
+      id: 1,
+      category: "E-commerce",
+      title: "Loja de Roupas",
+      description: "Lorem ipsum dolor sit amet...",
+      image: "url1.jpg",
+      stacks: ["React", "JavaScript", "Express", "MongoDB"],
+    },
+    {
+      id: 2,
+      category: "Delivery",
+      title: "App Delivery",
+      description: "Lorem ipsum dolor sit amet...",
+      image: "url2.jpg",
+      stacks: ["React", "Node.js", "Express", "MongoDB"],
+    },
+    {
+      id: 3,
+      category: "Dashboard",
+      title: "SaaS Dashboard",
+      description: "Lorem ipsum dolor sit amet...",
+      image: "url3.jpg",
+      stacks: ["React", "Python", "Docker"],
+    },
+  ];
 
   return (
-    <div className='min-h-80 mt-25 mb-15 flex flex-col items-center w-full'>
-        <div className="flex justify-between w-full max-w-[70%] mb-8">
-            <h3 className="font-main text-font text-xl font-medium">Alguns projetos</h3>
-            <a href="https://github.com/niethilleybrazz" target="_blank" rel="noopener noreferrer" className="flex items-center gap-3 text-font font-others">Ver todos <TiArrowForwardOutline className='font-medium'/></a>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-[85%] min-h-[70%] bg-main-bg border-borders">
-            {
-                projectsData.map((item)=>(
-                    <ProjectCard 
-                        key={item.id}
-                        image={item.image}
-                        category={item.category}
-                        title={item.title}
-                        description={item.description}
-                    />
-                ))
-            }
-        </div>
+    <div className="min-h-80 mt-25 mb-15 flex flex-col items-center w-full">
+      <div className="flex justify-between w-full max-w-[70%] mb-8">
+        <h3 className="font-main text-font text-xl font-medium">
+          Alguns projetos
+        </h3>
+        <a
+          href="https://github.com/niethilleybrazz"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-3 text-font font-others"
+        >
+          Ver todos <TiArrowForwardOutline className="font-medium" />
+        </a>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-[85%] min-h-[70%] bg-main-bg border-borders">
+        {projectsData.map((item) => (
+          <ProjectCard
+            key={item.id}
+            image={item.image}
+            category={item.category}
+            title={item.title}
+            description={item.description}
+            stacks={item.stacks}
+            stackColors={item.stackColors}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- my-portifolio/src/components/ui/ProjectCard.jsx
+++ my-portifolio/src/components/ui/ProjectCard.jsx
@@ -1,20 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const ProjectCard = ({image, category, title, description}) => { 
+const ProjectCard = ({ image, category, title, description, stacks }) => {
+
+  const stackColors = {
+    React: "border-react text-react",
+    "Node.js": "border-node text-node",
+    Express: "border-express text-express",
+    MongoDB: "border-mongodb text-mongodb",
+    Python: "border-python text-python",
+    Docker: "border-docker text-docker",
+    JavaScript: "border-javascript text-javascript",
+    Java: "border-java text-java",
+    "Tailwind CSS": "border-tailwind text-tailwind",
+  };
+  const [flipped, setFlipped] = useState(false);
+
   return (
-    <div className='border rounded-xl border-borders overflow-hidden px-10 py-10 pt-10 pb-10'>
-        <img src={image} alt={title} className='w-full h-48 object-cover rounded-md mb-3 text-font'/>
-        <span className='text-font-secondary font-others text-[12px]'>
+    <div
+      className='border rounded-xl border-borders overflow-hidden px-10 py-10 pt-10 pb-10 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 cursor-pointer '
+      onClick={() => setFlipped(!flipped)}
+    >
+      {!flipped && (
+        <>
+          <img
+            src={image}
+            alt={title}
+            className="w-full h-48 object-cover rounded-md mb-3 text-font"
+          />
+          <span className="text-font-secondary font-others text-[12px]">
             {category}
-        </span>
-        <h3 className='font-main text-font my-2 font-medium'>
-            {title}
-        </h3>
-        <p className="font-secondary text-font-secondary text-xs">
+          </span>
+          <h3 className="font-main text-font my-2 font-medium">{title}</h3>
+          <p className="font-secondary text-font-secondary text-xs">
             {description}
-        </p>
+          </p>
+        </>
+      )}
+      {flipped && (
+        <>
+          <h3 className="font-main text-font my-2 font-medium">{title}</h3>
+          <p className="font-others text-borders text-xs">Tecnologias usadas</p>
+          <div className="flex flex-wrap justify-center gap-3 mt-5">
+            {stacks.map((stack) => (
+              <span
+                key={stack}
+                className={`px-4 py-1 rounded-4xl border font-others text-xs transition-all ${stackColors[stack]}`}
+              >
+                {stack}
+              </span>
+            ))}
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
